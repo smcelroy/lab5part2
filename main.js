ea281b40519da9d3a8177417e7eb56d90f6ccc08
Ryan Lane <ryan@ryanalane.com>
2014-11-22 16:18:10 -0700

    Changed updateCity() from $.watch() to manual (button click) triggered

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
 function CityController($scope, $http) {
 	//initialize citiesStats
-	$scope.citiesStats = [];
-	
-	$http.get('http://api.openweathermap.org/data/2.5/weather?q=London').success(function(data){
-		$scope.citiesStats[0] = data;
-		$scope.citiesStats[0].main.temp -= 273.15;
-		console.log($scope.citiesStats[0].main.temp);
-	});
-	$http.get('http://api.openweathermap.org/data/2.5/weather?q=Phoenix').success(function(data){
-		$scope.citiesStats[1] = data;
-		$scope.citiesStats[1].main.temp -= 273.15;
-	});
-	$http.get('http://api.openweathermap.org/data/2.5/weather?q=Manhattan').success(function(data){
-		$scope.citiesStats[2] = data;
-		$scope.citiesStats[2].main.temp -= 273.15;
-		on = !on;
-	});
-	
+	$scope.citiesStats = [{name: 'London'},{name: 'Phoenix'},{name: 'New York'}];
+
 	$scope.calculateStats = function(){
 		console.log($scope.citiesStats[0].main.temp);
 		var avgTemp = 0;
@@ -68,10 +53,13 @@ function CityController($scope, $http) {
 		$http.get('http://api.openweathermap.org/data/2.5/weather?q='+$scope.citiesStats[index].name).success(function(data){
 			$scope.citiesStats[index] = data;
 			$scope.citiesStats[index].main.temp -= 273.15;
+			$scope.calculateStats();
 		});
 	}
 	
-	$scope.$watch('citiesStats', $scope.calculateStats, true);
+	for(var i = 0; i < $scope.citiesStats.length; ++i){
+		$scope.updateCity(i);
+	}
 }
 
 	
